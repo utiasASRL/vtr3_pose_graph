c1386188696081e04ef68e85850ef634900c5d48
a-krawciw <freakyfactoid@gmail.com>
2025-10-09 18:29:51 -0400

    Add pose graph info script

---
+++ samples/print_pose_graph_info.py
@@ -0,0 +1,40 @@
+import os
+import numpy as np
+
+from vtr_utils.bag_file_parsing import Rosbag2GraphFactory
+
+from vtr_pose_graph.graph_iterators import TemporalIterator
+import vtr_pose_graph.graph_utils as g_utils
+import argparse
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+                        prog = 'Plot Repeat Path',
+                        description = 'Plots scatter of points to show path. Also calculates RMS error')
+    parser.add_argument('graph', help="The filepath to the pose graph folder. (Usually /a/path/graph)")
+    args = parser.parse_args()
+
+    factory = Rosbag2GraphFactory(args.graph)
+
+    graph = factory.buildGraph()
+    print(f"Graph {graph} has {graph.number_of_vertices} vertices and {graph.number_of_edges} edges")
+
+
+    for i in range(graph.major_id):
+        v_start = graph.get_vertex((i,0))
+
+        x = []
+        y = []
+        t = []
+        dist = []
+        path_len = 0
+
+        for v, e in TemporalIterator(v_start):
+            x.append(v.T_v_w.r_ba_ina()[0])
+            y.append(v.T_v_w.r_ba_ina()[1])
+            t.append(v.stamp / 1e9)
+            path_len += np.linalg.norm(e.T.r_ba_ina())
+        
+        print(f"Path {i+1} was {path_len:.3f}m long with {len(x)} vertices and is a {'teach' if v.taught else 'repeat'}.")
+
